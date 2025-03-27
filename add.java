bc05b0cc082a9979a0468b0319fa15d48fb0ecb1
prekshac083 <160208916+prekshac083@users.noreply.github.com>
2025-03-27 14:23:50 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,25 @@
+// Java Program to Sum of Three Numbers
+import java.io.*;
+ 
+class add {
+    public static void main (String[] args) {
+          // Three Integers
+        int a=1,b=2,c=3;
+            
+          // Method 1:
+       
+          // Adding sum of a+b and then
+          // sum of a+b with c
+          int k=a+b;
+          int result=k+c;
+       
+          // Printing to Sum
+          System.out.println("Method 1: Sum a+b+c = "+result);
+           
+          // Method 2:
+       
+          // Printing the Sum of Three
+          // Variables(Numbers)
+          System.out.println("Method 2: Sum a+b+c = "+(a+b+c));    
+    }
+}
